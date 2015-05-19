2d1625bd8871a6a9718d346a9871e10325cc08d7
Esteban <stedeg_3@hotmail.com>
2015-05-19 23:28:14 +0200

    Arreglado AgendaEJB-query

--- SSCEntrega3-ejb/src/ejb/AgendaEJB.java
+++ SSCEntrega3-ejb/src/ejb/AgendaEJB.java
@@ -7,13 +7,16 @@ package ejb;
 
 import java.util.LinkedList;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import model.jpa.ssc.Cita;
+import model.jpa.ssc.Profesional;
 
 /**
  *
@@ -27,10 +30,18 @@ public class AgendaEJB {
 
     public List<Cita> getCitas(long idProfesional){
         List<Cita> l = new LinkedList<>();
-        TypedQuery<Cita> q = em.createQuery("Select c from Cita c where PROFESIONAL_ID = :idProf",Cita.class);
-        for(Cita c : q.setParameter("idProf", idProfesional).getResultList()){
-            l.add(c);
+        /*
+        Query q = em.createQuery("SELECT c FROM Cita c");
+        l.addAll(q.getResultList());
+        */
+        Profesional p = em.find(Profesional.class,idProfesional);
+        if(p != null){
+            TypedQuery<Cita> q = em.createQuery("SELECT c FROM Cita c WHERE c.profesional = :idProf",Cita.class);
+            for(Cita c : q.setParameter("idProf", p).getResultList()){
+                l.add(c);
+            }
         }
+        
         return l;
     }
 }
