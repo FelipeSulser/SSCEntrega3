14251e9d12eeec4db37705b51cb76b7397e98e3b
Haritz <haritzpuerto94@gmail.com>
2015-05-23 20:52:33 +0200

    Error control added

--- SSCEntrega3-ejb/src/ejb/CrearCitaEJB.java
+++ SSCEntrega3-ejb/src/ejb/CrearCitaEJB.java
@@ -6,13 +6,16 @@
 package ejb;
 
 import exceptions.CiudadanoNotFoundException;
+import exceptions.CrearCitaException;
 import exceptions.ProfesionalNotFoundException;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TransactionRequiredException;
 import javax.persistence.TypedQuery;
 import model.jpa.ssc.Cita;
 import model.jpa.ssc.Ciudadano;
@@ -40,7 +43,7 @@ public class CrearCitaEJB {
             query = em.createQuery(stringQuery, Ciudadano.class);
             ciudadano = (Ciudadano) query.getSingleResult();
             return ciudadano;
-        } catch(NoResultException e){
+        } catch(NoResultException | IllegalArgumentException e){
             throw new CiudadanoNotFoundException(DNICiudadano);
         }
     }
@@ -52,18 +55,28 @@ public class CrearCitaEJB {
             query = em.createQuery(stringQuery, Profesional.class);
             profesional = (Profesional) query.getSingleResult();
             return profesional;
-        }catch(NoResultException e){
+        }catch(NoResultException | IllegalArgumentException e){
             throw new ProfesionalNotFoundException(DNIProfesional);
         }
     }
 
-    public void setCita(Cita cita) {
-        em.persist(cita);
+    public void setCita(Cita cita) throws CrearCitaException {
+        try{
+            em.persist(cita);
+        }catch(EntityExistsException e){
+            throw new CrearCitaException("Ya existe esa cita.");
+        }catch(IllegalArgumentException | TransactionRequiredException e ){
+            throw new CrearCitaException("Error al crear la cita.");
+        }
     }
 
-    public Long getCitaId(Cita cita) {
-        cita = em.merge(cita);
-        return cita.getId();
+    public Long getCitaId(Cita cita) throws CrearCitaException {
+        try{
+            cita = em.merge(cita);
+            return cita.getId();
+        }catch(IllegalArgumentException | TransactionRequiredException e ){
+            throw new CrearCitaException("Error al crear la cita.");
+        }
     }
     
     
