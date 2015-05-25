a550691e8d5b2d51155c931a2792cbcab5395006
Esteban <stedeg_3@hotmail.com>
2015-05-25 22:28:18 +0200

    eliminados unused imports ScheduleView

--- SSCEntrega3-war/src/java/controller/ScheduleView.java
+++ SSCEntrega3-war/src/java/controller/ScheduleView.java
@@ -17,13 +17,11 @@ import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import javax.inject.Named;
 import model.jpa.ssc.Cita;
 import model.jpa.ssc.EstadoCita;
 
