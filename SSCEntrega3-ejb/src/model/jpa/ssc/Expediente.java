4e7662a3ce85d359d9fbcee9ce705f1b0bbd93e2
Esteban <stedeg_3@hotmail.com>
2015-05-25 21:24:34 +0200

    eliminados imports no usados Expediente (entidad)

--- SSCEntrega3-ejb/src/model/jpa/ssc/Expediente.java
+++ SSCEntrega3-ejb/src/model/jpa/ssc/Expediente.java
@@ -14,10 +14,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Transient;
  
 /**
  *
